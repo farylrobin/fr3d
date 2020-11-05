057a4e37303313e47cfd803f8c2e3145466af8c6
gabebiolos <>
2020-11-05 14:53:32 -0500

    config-up

---
+++ aola-config-test-5/keyshot-configurator-imagemapping.js
@@ -0,0 +1,69 @@
+imagemappingdata =
+[
+    {
+        "image": "aola-config-test-5.1.png",
+        "modelsets": [
+            1005,
+            604184836
+        ],
+        "multimaterials": [
+            "111+313"
+        ],
+        "studio": 960352158
+    },
+    {
+        "image": "aola-config-test-5.2.png",
+        "modelsets": [
+            1005,
+            604184836
+        ],
+        "multimaterials": [
+            "111+316"
+        ],
+        "studio": 960352158
+    },
+    {
+        "image": "aola-config-test-5.3.png",
+        "modelsets": [
+            1005,
+            604184836
+        ],
+        "multimaterials": [
+            "111+313"
+        ],
+        "studio": 604185939
+    },
+    {
+        "image": "aola-config-test-5.4.png",
+        "modelsets": [
+            1005,
+            604184836
+        ],
+        "multimaterials": [
+            "111+316"
+        ],
+        "studio": 604185939
+    },
+    {
+        "image": "aola-config-test-5.5.png",
+        "modelsets": [
+            1005,
+            604184836
+        ],
+        "multimaterials": [
+            "111+313"
+        ],
+        "studio": 604186157
+    },
+    {
+        "image": "aola-config-test-5.6.png",
+        "modelsets": [
+            1005,
+            604184836
+        ],
+        "multimaterials": [
+            "111+316"
+        ],
+        "studio": 604186157
+    }
+];
---
+++ aola-config-test-5/keyshot-configurator-modeldata.js
@@ -0,0 +1,79 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "slate.png",
+                    "multimaterials": [
+                        "111+313"
+                    ],
+                    "name": "Slate"
+                },
+                {
+                    "icon": "beige.png",
+                    "multimaterials": [
+                        "111+316"
+                    ],
+                    "name": "Beige"
+                }
+            ],
+            "name": "Suade"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "solo.png",
+            "materialgroups": [
+                "Suade"
+            ],
+            "modelset": 1005,
+            "name": "Solo",
+            "productoptiongroups": [
+                {
+                    "name": "Solo",
+                    "productoptions": [
+                        {
+                            "checked": true,
+                            "name": "None"
+                        }
+                    ]
+                },
+                {
+                    "name": "Pair",
+                    "productoptions": [
+                        {
+                            "checked": true,
+                            "icon": "pair.png",
+                            "materialgroups": [
+                                "Suade"
+                            ],
+                            "modelset": 604184836,
+                            "name": "Pair"
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "3q.png",
+            "id": 604185939,
+            "name": "3Q"
+        },
+        {
+            "icon": "profile.png",
+            "id": 604186157,
+            "name": "Profile"
+        },
+        {
+            "icon": "pair-2.png",
+            "id": 960352158,
+            "name": "Pair"
+        }
+    ]
+};
